ed52afd852ac106396a3ff561fd0c3ca3b51601f
swardbladpaulo <paulo@swardblads.se>
2021-01-26 11:57:33 +0100

    add login-btn to menuheader

--- src/components/MenuHeader.jsx
+++ src/components/MenuHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Menu, Segment } from "semantic-ui-react"
+import { Menu, Segment, Button, Icon } from "semantic-ui-react"
 import { Link } from 'react-router-dom'
 
 const MenuHeader = () => {
@@ -9,6 +9,17 @@ const MenuHeader = () => {
       <Menu inverted>
         <Menu.Item as={Link} to={{ pathname: "/" }}>
           <h1>MenuHeaderLogo</h1>
+        </Menu.Item>
+        <Menu.Item position="right">
+          <Button
+            icon
+            labelPosition="left"
+            icon="user"
+            data-cy="login-btn"          
+          >
+            <Icon name="user"></Icon>
+            Login
+          </Button>
         </Menu.Item>
 
       </Menu>
