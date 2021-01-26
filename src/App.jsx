9e7be932657d6f99ca1f1936e93caf681e317c85
swardbladpaulo <paulo@swardblads.se>
2021-01-26 11:43:54 +0100

    add package semantic ui, react-router-dom, add menuheader component, add browserrouter to index.js, modify index.js, display menuheader in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
+import MenuHeader from './components/MenuHeader'
 
 const App = () => {
   const message = useSelector(state=> state.greeting)
   return (
     <>
+      <MenuHeader />
       <h1>{message}</h1>
+      
     </>
   )
 }
---
+++ src/components/MenuHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Menu, Segment } from "semantic-ui-react"
+import { Link } from 'react-router-dom'
+
+const MenuHeader = () => {
+
+  return (
+    <Segment inverted>
+      <Menu inverted>
+        <Menu.Item as={Link} to={{ pathname: "/" }}>
+          <h1>MenuHeaderLogo</h1>
+        </Menu.Item>
+
+      </Menu>
+
+    </Segment>
+  )
+}
+
+export default MenuHeader
