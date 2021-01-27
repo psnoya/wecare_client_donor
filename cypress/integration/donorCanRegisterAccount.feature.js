15e30d963959c6db29f77fff7ee5dfb9731ea5eb
psnoya <paulina.s.noya@gmail.com>
2021-01-27 10:48:43 +0100

    Adding error message

--- cypress/integration/donorCanRegisterAccount.feature.js
+++ cypress/integration/donorCanRegisterAccount.feature.js
@@ -3,7 +3,7 @@ describe('Donor can register an account', () => {
     cy.server()
     cy.route({
       method: 'POST',
-      url: 'http://localhost:3000/api/**',
+      url: 'http://localhost:3000/api/auth',
       response: 'fx:donor_can_register.json',
       headers: {
         uid: 'donor@donor.com',
@@ -24,11 +24,11 @@ describe('Donor can register an account', () => {
       cy.get("[data-cy='email']").type('donor@donor.com')
       cy.get("[data-cy='password']").type('123456')
       cy.get("[data-cy='password-confirmation']").type('123456')
-      cy.get("[data-cy='submit-btn']").click()
       cy.get("[data-cy='header-user-email']").should(
         'contain',
         'Welcome donor@donor.com'
       )
+      cy.get("[data-cy='submit-btn']").click()
       cy.get("[data-cy='register-btn']").should('not.be.visible')
     })
   })
@@ -44,6 +44,18 @@ describe('Donor can register an account', () => {
           },
           success: false,
         },
+      })
+      cy.visit('/')
+      cy.get("[data-cy='register-btn']").click()
+      cy.get("[data-cy='registration-form']").within(() => {
+        cy.get("[data-cy='email']").type('donor@donor.com')
+        cy.get("[data-cy='password']").type('password')
+        cy.get("[data-cy='password-confirmation']").type('wrong_password')
+        cy.get("[data-cy='submit-btn']").click()
+        cy.get("[data-cy='register-error-message']").should(
+          'contain',
+          'Invalid login credentials. Please try again.'
+        )
       })
 
       it('Password confirmation dont match', () => {
