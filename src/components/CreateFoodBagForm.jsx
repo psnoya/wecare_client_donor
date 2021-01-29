d74a655645d5913bc7468cf9d9830b0a0517023f
flis_ann <ann.flismark@gmail.com>
2021-01-29 10:28:49 +0100

    Adding test for finding radio buttons Adding radio buttons

--- src/components/CreateFoodBagForm.jsx
+++ src/components/CreateFoodBagForm.jsx
@@ -6,13 +6,14 @@ import {
   Header,
   Icon,
   Message,
+  Radio,
 } from 'semantic-ui-react'
 import { useDispatch, useSelector } from 'react-redux'
 import FoodBagServices from '../modules/FoodBagServices'
 
 const CreateFoodBagForm = () => {
   const dispatch = useDispatch()
-  const { createFoodBagMessage, errorMessage } = useSelector((state) => state)
+  const { createFoodBagMessage, errorMessage } = useSelector(state => state)
 
   return (
     <Container>
@@ -24,8 +25,36 @@ const CreateFoodBagForm = () => {
       />
       <Form
         data-cy='foodbag-form'
-        onSubmit={(event) => FoodBagServices.create(event, dispatch)}
+        onSubmit={event => FoodBagServices.create(event, dispatch)}
       >
+        <Form.Field label='Please select a pickup time' />
+        <Form.Field>
+          <Radio
+            data-cy='pickup-time1'
+            label='08-12'
+            name='morning'
+            value='morning'
+            // checked={}
+            // onChange={}
+          />
+          <Radio
+            data-cy='pickup-time2'
+            label='12-16'
+            name='afternoon'
+            value='afternoon'
+            // checked={}
+            // onChange={}
+          />
+          <Radio
+            data-cy='pickup-time3'
+            label='16-20'
+            name='evening'
+            value='evening'
+            // checked={}
+            // onChange={}
+          />
+        </Form.Field>
+
         <Button data-cy='submit-btn' type='submit' color='purple' icon='heart'>
           <Icon name='heart'></Icon>
           Donate
