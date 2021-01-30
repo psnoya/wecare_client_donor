e61a6617c498e07851f02d7dfb0f514b9e7175fc
flis_ann <ann.flismark@gmail.com>
2021-01-30 13:37:56 +0100

    Cleaning up code, now Register button is hidden when donor ha registered. Donor is taken to Create Foodbag

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,18 @@ import React, { Suspense } from 'react'
 import MenuHeader from './components/MenuHeader'
 import Footer from './components/Footer'
 import './index.css'
-import { Route, Switch } from 'react-router-dom'
 import MainPageDisplay from './components/MainPageDisplay'
 import CreateFoodBagForm from './components/CreateFoodBagForm'
-import { useSelector } from "react-redux";
-
-// import DonorDashBoard from './components/DonorDashBoard'
+import { useSelector } from 'react-redux'
 
 const App = () => {
-  const currentUser = useSelector(state => state)
+  const currentUser = useSelector(state => state.currentUser)
 
   return (
     <>
       <Suspense fallback={<h3>Loading...</h3>}>
         <MenuHeader />
-        {/* <MainPageDisplay /> */}
-        {/* <DonorDashBoard /> */}
-        {!currentUser && <CreateFoodBagForm />}
-
-        <Switch>
-          <Route exact path='/' component={MainPageDisplay}></Route>
-          <Route exact path='/foodbag' component={CreateFoodBagForm}></Route>
-        </Switch>
+        {currentUser ? <CreateFoodBagForm /> : <MainPageDisplay />}
         <Footer />
       </Suspense>
     </>
--- src/components/MenuHeader.jsx
+++ src/components/MenuHeader.jsx
@@ -1,36 +1,19 @@
 import React from 'react'
-import { useSelector } from "react-redux";
-import { Button, Image, Menu, Segment } from 'semantic-ui-react'
-import { Link, NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Image, Menu, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 import RegistrationForm from './RegistrationForm'
-import CreateFoodBagForm from './CreateFoodBagForm'
 
 const MenuHeader = () => {
-  const currentUser = useSelector(state => state)
+  const currentUser = useSelector(state => state.currentUser)
   return (
     <Segment inverted>
       <Menu inverted>
         <Menu.Item as={Link} to={{ pathname: '/' }}>
           <Image src='./img/wecare_logo.png' size='medium' />
         </Menu.Item>
-        {/* <Menu.Item
-          id='foodbag'
-          name='create foodbag'
-          as={NavLink}
-          to={{ pathname: '/foodbag' }}
-        ></Menu.Item> */}
         <Menu.Item position='right'>
           {!currentUser && <RegistrationForm />}
-          {currentUser && currentUser.data.role === 'registered_user' && (
-            <CreateFoodBagForm />
-          )}
-        </Menu.Item>
-
-        <Menu.Item position='right'>
-          <RegistrationForm />
-          <Button size='small' position='middle' color='blue'>
-            Login Here
-          </Button>
         </Menu.Item>
       </Menu>
     </Segment>
