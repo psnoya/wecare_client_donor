53ef59b02f3461a70362cc11735d8dc5685c0db9
flis_ann <ann.flismark@gmail.com>
2021-01-30 11:44:56 +0100

    Trying to figure out locig

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import './index.css'
 import { Route, Switch } from 'react-router-dom'
 import MainPageDisplay from './components/MainPageDisplay'
 import CreateFoodBagForm from './components/CreateFoodBagForm'
+import { useSelector } from "react-redux";
+
 // import DonorDashBoard from './components/DonorDashBoard'
 
 const App = () => {
+  const currentUser = useSelector(state => state)
+
   return (
     <>
       <Suspense fallback={<h3>Loading...</h3>}>
         <MenuHeader />
         {/* <MainPageDisplay /> */}
         {/* <DonorDashBoard /> */}
+        {!currentUser && <CreateFoodBagForm />}
+
         <Switch>
           <Route exact path='/' component={MainPageDisplay}></Route>
           <Route exact path='/foodbag' component={CreateFoodBagForm}></Route>
--- src/components/MenuHeader.jsx
+++ src/components/MenuHeader.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
+import { useSelector } from "react-redux";
 import { Button, Image, Menu, Segment } from 'semantic-ui-react'
 import { Link, NavLink } from 'react-router-dom'
 import RegistrationForm from './RegistrationForm'
-import DonorDashBoard from './DonorDashBoard'
+import CreateFoodBagForm from './CreateFoodBagForm'
 
 const MenuHeader = () => {
+  const currentUser = useSelector(state => state)
   return (
     <Segment inverted>
       <Menu inverted>
         <Menu.Item as={Link} to={{ pathname: '/' }}>
           <Image src='./img/wecare_logo.png' size='medium' />
         </Menu.Item>
-        <Menu.Item
-        id='foodbag'
-        name='create foodbag'
-        as={NavLink}
-        to={{ pathname: '/foodbag'}}
-        >
-          
+        {/* <Menu.Item
+          id='foodbag'
+          name='create foodbag'
+          as={NavLink}
+          to={{ pathname: '/foodbag' }}
+        ></Menu.Item> */}
+        <Menu.Item position='right'>
+          {!currentUser && <RegistrationForm />}
+          {currentUser && currentUser.data.role === 'registered_user' && (
+            <CreateFoodBagForm />
+          )}
         </Menu.Item>
+
         <Menu.Item position='right'>
           <RegistrationForm />
           <Button size='small' position='middle' color='blue'>
