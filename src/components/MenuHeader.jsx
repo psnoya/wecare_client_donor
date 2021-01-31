2e3b1eebfe3af825753d5d8973ffa85425a12c67
Ehugo2000 <erik.hgsn@gmail.com>
2021-01-31 14:48:17 +0100

    removing button.

--- src/components/MenuHeader.jsx
+++ src/components/MenuHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Button, Image, Menu, Segment } from 'semantic-ui-react'
+import { Image, Menu, Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import RegistrationForm from './RegistrationForm'
 
