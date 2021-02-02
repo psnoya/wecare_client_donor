a15f1816f9ea61cd6ae1e6f683f4c7730824f425
swardbladpaulo <paulo@swardblads.se>
2021-02-02 14:38:22 +0100

    removed unneccessary code

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector} from 'react-redux'
 import { Form, Button, Input, Message, Container, Segment, Header } from 'semantic-ui-react'
 import ProfileServices from '../modules/ProfileServices'
@@ -9,10 +9,6 @@ const ProfileForm = () => {
   const dispatch = useDispatch()
   const updateProfileMessage = useSelector((state) => state.updateProfileMessage)
   const currentUser = useSelector(state => state.currentUser)
-  const [companyName, setCompanyName] = useState()
-  const [adress, setAdress] = useState()
-  const [zipcode, setZipcode] = useState()
-  const [city, setCity] = useState()
 
   return (
     <Container>
@@ -32,7 +28,6 @@ const ProfileForm = () => {
         control={Input}
         name='companyName'
         placeholder='Company Name'
-        onChange={(event) => setCompanyName(event.target.value)}
       />
       <Form.Field
         data-cy='adress'
@@ -40,7 +35,6 @@ const ProfileForm = () => {
         control={Input}
         name='adress'
         placeholder='Adress'
-        onChange={(event) => setAdress(event.target.value)}
       />
       <Form.Field
         data-cy='zipcode'
@@ -48,7 +42,6 @@ const ProfileForm = () => {
         control={Input}
         name='zipcode'
         placeholder='Zipcode'
-        onChange={(event) => setZipcode(event.target.value)}
       />
       <Form.Field
         data-cy='city'
@@ -56,7 +49,6 @@ const ProfileForm = () => {
         control={Input}
         name='city'
         placeholder='City'
-        onChange={(event) => setCity(event.target.value)}
       />
         <Button data-cy='submit-btn' type='save' color='pink' value='save'>
       Save
